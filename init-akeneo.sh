06f7b493ec3eae03bad48f8b4067bf63fafd6953
Vincent ARROCENA <varrocen@gmail.com>
2015-03-26 00:05:15 +0100

    Add fig.yml and fix vhost file

--- init-akeneo.sh
+++ init-akeneo.sh
@@ -5,6 +5,7 @@ cd /var/www/pim-community-standard-v1.3.2-icecat
 sed -i 's/localhost/'$LINK_ALIAS'/' app/config/parameters.yml
 php app/console cache:clear --env=prod
 php app/console pim:install --env=prod
+chown -R www-data:www-data /var/www
 
 apache2ctl -D FOREGROUND
 
